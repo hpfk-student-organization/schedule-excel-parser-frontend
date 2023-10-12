c01707ffb9cf4b5f6520a254541cf206b5f218d4
Dugd_Nitkin <dugd.nitkin@gmail.com>
2023-10-12 22:16:44 +0300

    create get response prototype

--- site/assets/js/upload.js
+++ site/assets/js/upload.js
@@ -1,15 +1,39 @@
+const API_URL = './timetable.json'
+const EDIT_URL = './edit'
+const loading = document.getElementById('loading');
 const fileInput = document.getElementById('fileInput');
 const dropArea = document.getElementById('dropArea');
-const fileInfo = document.getElementById('fileInfo');
+
+function sendFile(file) {
+    fetchAPI()
+        .then(data => responseAPI(data))
+        .catch(error => {
+            console.error(error);
+        });
+    loading.innerHTML = "Загрузка..."
+}
+
+function fetchAPI() {
+    return fetch(API_URL)
+        .then(response => {
+            if (response.ok) {
+                return response.json();
+            } else {
+                throw new Error('Не удалось загрузить JSON файл');
+            }
+        })
+}
+
+function responseAPI(data) {
+    sessionStorage.setItem('timetable', JSON.stringify(data));
+    setTimeout(() => window.open(EDIT_URL, '_self'), 1000);
+}
 
 fileInput.addEventListener('change', function () {
     const file = fileInput.files[0];
-    displayFileInfo(file);
+    sendFile(file);
+    fileInput.value = "";
 });
-
-function displayFileInfo(file) {
-    fileInfo.innerHTML = `Iм'я файла: ${file.name}, Розмір: ${file.size} байт`;
-}
 
 dropArea.addEventListener('dragover', function (e) {
     e.preventDefault();
@@ -24,5 +48,5 @@ dropArea.addEventListener('drop', function (e) {
     e.preventDefault();
     dropArea.style.border = '2px dashed #ccc';
     const file = e.dataTransfer.files[0];
-    displayFileInfo(file);
+    sendFile(file);
 });
